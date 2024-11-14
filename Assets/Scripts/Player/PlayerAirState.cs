d62a66c574843e0018447d00be5a1218a33bf542
Rocky <rocky.haung.gamedev@gmail.com>
2024-11-14 17:10:15 +0800

    feat: implement jump counter in air state, update jump logic and animations

--- Assets/Scripts/Player/PlayerAirState.cs
+++ Assets/Scripts/Player/PlayerAirState.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public abstract class PlayerAirState : PlayerState
 {
+
+    int jumpCounter;
+
     public PlayerAirState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
     {
     }
@@ -12,6 +15,7 @@ public abstract class PlayerAirState : PlayerState
     {
         base.Enter();
         stateTimer = player.jumpCantMoveDuration;
+        jumpCounter = player.Anim.GetInteger("JumpCounter");
     }
 
     public override void Exit()
@@ -26,10 +30,21 @@ public abstract class PlayerAirState : PlayerState
         if (xInput != 0 && stateTimer < 0)
             player.SetVelocity(player.moveSpeed * .8f * xInput, rb.velocity.y);
 
+
+        if (player.IsGroundDetected())
+            player.Anim.SetInteger("JumpCounter", 0);
+
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCounter < 1)
+        {
+            player.Anim.SetInteger("JumpCounter", jumpCounter + 1);
+            stateMachine.ChangeState(player.Jump);
+        }
+
         if (Input.GetKeyDown(KeyCode.LeftShift))
             stateMachine.ChangeState(player.Roll);
 
         if (Input.GetKeyDown(KeyCode.Mouse0))
             stateMachine.ChangeState(player.AirAttack);
+
     }
 }
