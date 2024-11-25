0a087dc5353cbf96f807eb3c8fcbc84e433a03d6
Rocky <rocky.haung.gamedev@gmail.com>
2024-11-25 23:29:15 +0800

    feat: enhance GameLevelManager to support multiple levels and enemy types

--- Assets/Scripts/Manager/GameLevelManager.cs
+++ Assets/Scripts/Manager/GameLevelManager.cs
@@ -6,8 +6,17 @@ public class GameLevelManager : MonoBehaviour
 {
     public static GameLevelManager instance;
     [SerializeField] private GameObject level0;
+    [SerializeField] private GameObject merchant;
     [SerializeField] private GameObject[] level1;
     [SerializeField] private GameObject[] level1Enemies;
+    [SerializeField] private GameObject[] level1Bosses;
+    [SerializeField] private GameObject[] level2;
+    [SerializeField] private GameObject[] level2Enemies;
+    [SerializeField] private GameObject[] level2Bosses;
+    [SerializeField] private GameObject[] level3;
+    [SerializeField] private GameObject[] level3Enemies;
+    [SerializeField] private GameObject[] level3Bosses;
+    [SerializeField] private GameObject finalBoss;
     [SerializeField] private GameObject spawnPoint;
     public int currentLevel = 0;
 
@@ -62,24 +71,18 @@ public class GameLevelManager : MonoBehaviour
 
         yield return new WaitForSeconds(0.5f);
 
-        bool swordSelected = false;
-        UIManager.instance.swordSelect.ShowSwordSelect(() => swordSelected = true);
-        yield return new WaitUntil(() => swordSelected);
-        
+        if (currentLevel != 4 && currentLevel != 9 && currentLevel != 14)
+        {
+            bool swordSelected = false;
+            UIManager.instance.swordSelect.ShowSwordSelect(() => swordSelected = true);
+            yield return new WaitUntil(() => swordSelected);
+        }
+
         ClearLevel();
         currentLevel++;
         TransitionManager.instance.transition.EndTransition();
 
-        if (currentLevel < 4)
-        {
-            int randomIndex = Random.Range(0, level1.Length);
-            level1[randomIndex].SetActive(true);
-        }
-        else if (currentLevel == 4)
-        {
-            level0.SetActive(true);
-            currentLevel = 0;
-        }
+        SetupStage();
 
         yield return new WaitForSeconds(1f);
         player.Rb.bodyType = RigidbodyType2D.Dynamic;
@@ -87,16 +90,94 @@ public class GameLevelManager : MonoBehaviour
         yield return new WaitForSeconds(1f);
         if (currentLevel != 0)
         {
-            SpawnRandomEnemy();
+            SpawnRandomEnemy(currentLevel);
         }
     }
 
-    private void SpawnRandomEnemy()
+    private void SetupStage()
     {
-        int randomIndex = Random.Range(0, level1Enemies.Length);
-        Vector3 spawnPosition = spawnPoint.transform.position;
-        GameObject enemy = Instantiate(level1Enemies[randomIndex], spawnPosition, Quaternion.identity);
-        FacePlayer(enemy);
+        switch (currentLevel)
+        {
+            case >= 1 and <= 3:
+                {
+                    int randomIndex = Random.Range(0, level1.Length);
+                    level1[randomIndex].SetActive(true);
+                    break;
+                }
+            case 5:
+                {
+                    int randomIndex = Random.Range(0, level1.Length);
+                    level1[randomIndex].SetActive(true);
+                    break;
+                }
+            case >= 6 and <= 8:
+                {
+                    int randomIndex = Random.Range(0, level2.Length);
+                    level2[randomIndex].SetActive(true);
+                    break;
+                }
+            case 10:
+                {
+                    int randomIndex = Random.Range(0, level2.Length);
+                    level2[randomIndex].SetActive(true);
+                    break;
+                }
+            case >= 11 and <= 13:
+                {
+                    int randomIndex = Random.Range(0, level3.Length);
+                    level3[randomIndex].SetActive(true);
+                    break;
+                }
+            case 15:
+                {
+                    int randomIndex = Random.Range(0, level3.Length);
+                    level3[randomIndex].SetActive(true);
+                    break;
+                }
+            case 16:
+                break;
+            case 4 or 9 or 14:
+                merchant.SetActive(true);
+                break;
+        }
+    }
+
+    private void SpawnRandomEnemy(int currentLevel)
+    {
+        GameObject[] enemiesArray = null;
+
+        switch (currentLevel)
+        {
+            case >= 1 and <= 3:
+            enemiesArray = level1Enemies;
+            break;
+            case 5:
+            enemiesArray = level1Bosses;
+            break;
+            case >= 6 and <= 8:
+            enemiesArray = level2Enemies;
+            break;
+            case 10:
+            enemiesArray = level2Bosses;
+            break;
+            case >= 11 and <= 13:
+            enemiesArray = level3Enemies;
+            break;
+            case 15:
+            enemiesArray = level3Bosses;
+            break;
+            case 16:
+            enemiesArray = new GameObject[] { finalBoss };
+            break;
+        }
+
+        if (enemiesArray != null && enemiesArray.Length > 0)
+        {
+            int randomIndex = Random.Range(0, enemiesArray.Length);
+            Vector3 spawnPosition = spawnPoint.transform.position;
+            GameObject enemy = Instantiate(enemiesArray[randomIndex], spawnPosition, Quaternion.identity);
+            FacePlayer(enemy);
+        }
     }
 
     private void FacePlayer(GameObject enemy)
