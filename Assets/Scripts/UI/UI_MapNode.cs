e339961c56098eeff002e3984c12c9391da911ca
Rocky <rocky.haung.gamedev@gmail.com>
2024-12-05 14:03:25 +0800

    feat: enhance UI_MapNode with color coding for node types and add fade animations for scene transitions

--- Assets/Scripts/UI/UI_MapNode.cs
+++ Assets/Scripts/UI/UI_MapNode.cs
@@ -27,9 +27,26 @@ public class UI_MapNode : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
     {
         node = _node;
 
-        if (node.Type == NodeType.Empty)
+        switch (node.Type)
         {
-            icon.color = new Color(0, 0, 0, 0); // Set color to transparent
+            case NodeType.Money:
+                icon.color = Color.yellow;
+                break;
+            case NodeType.Stats:
+                icon.color = Color.green;
+                break;
+            case NodeType.Rune:
+                icon.color = Color.blue;
+                break;
+            case NodeType.Shop:
+                icon.color = Color.magenta;
+                break;
+            case NodeType.Boss:
+                icon.color = Color.red;
+                break;
+            case NodeType.Empty:
+                icon.color = new Color(0, 0, 0, 0); // Set color to transparent
+                break;
         }
     }
 
